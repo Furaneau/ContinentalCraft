27b8e0168eb2c0441658b96b8d2c97855a018f0a
Furaneau <Furaneau@users.noreply.github.com>
2020-05-02 19:07:01 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,8 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, msg, args) => {
   let hEmbed = new Discord.MessageEmbed()
   .setColor('ff0000')
-  .setTitle('Commande bot')
-  .setDescription('Besoin du bot')
+  .setTitle('ContinentalCraft.eu')
+  .setDescription("Besoin d'aide")
   .addField('Commandes:', '- Ip: ******* ')
   .setFooter("J'espère que ça vous sera utile");
   msg.channel.send(hEmbed);
