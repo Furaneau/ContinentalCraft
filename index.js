fecd0cccd418a13fb6427f000a644ea29accad13
Furaneau <Furaneau@users.noreply.github.com>
2020-05-02 07:07:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,6 @@ const Discord = require('discord.js');
 const bot = new Discord.Client({DisableEveryone: true});
 const confid = require('./config.js');
 
-process.on('unhandledRejection', function(err) {
-    console.log(err);
-    // sendInTheCalvary(err);
-});
-
-const promise1 = new Promise(function(resolve, reject) {
-    throw new Error('Promise 1 has tanked.');
-});
-
-const promise2 = new Promise(function(resolve, reject) {
-    promise1.then();
-});
-
-(async function() {
-    try {
-        await promise2;
-    } catch(err) {
-        console.log(err);
-    }
-})();
-
 bot.on('ready', async () =>{
   console.log('Le bot est lancé.');
   bot.user.setActivity('continentalcraft.eu');
@@ -31,7 +10,7 @@ bot.on('ready', async () =>{
 bot.on('message', async (msg) => {
       if(msg.content.startsWith(config.prefix) && !msg.author.bot){
           cmdArray = msg.content.substring(1).split("")
-          cmd = cmdArray[1]
+          cmd = cmdArray[0]
           args = cmdArray.slice(1)
 
           msg.channel.send(cmd)
