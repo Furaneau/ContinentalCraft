ea6b014a24710dfb499096877680e0a29ccb34c9
Furaneau <Furaneau@users.noreply.github.com>
2020-05-02 05:23:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const discord = require('discord.js');
 const bot = new Discord.Client({DisableEveryone: true});
 
-bot.on('ready', asycn () =>{
+bot.on('ready', async () =>{
   bot.user.setActivity('Hey hey hey');
 });
 
