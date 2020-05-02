db648fb867ef5d2ef64817bd62fc6df6d6ef3b94
Furaneau <Furaneau@users.noreply.github.com>
2020-05-02 16:25:47 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -4,7 +4,7 @@ client.on('message', function (message) {
     let args = message.content.trim().split(/ +/g)
 
     if (args[0].toLowerCase() === prefix + 'kick') {
-       if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande ;(")
+       if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send("Vous navez pas la permission d utiliser cette commande");
        let member = message.mentions.members.first()
        if (!member) return message.channel.send("Veuillez mentionner un utilisateur :x:")
        if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("Vous ne pouvez pas kick cet utilisateur :x:")
