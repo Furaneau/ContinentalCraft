57a8c368d4439d66f2aaa0ef50160aa13733728d
Furaneau <Furaneau@users.noreply.github.com>
2020-05-02 06:29:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ bot.on('message', async (msg) => {
 });
 
 bot.on('message', async (msg) => {
-  if(msg.content.startWith(config.prefix && !msg.author.bot){
+  if(msg.content.startWith(config.prefix) && !msg.author.bot){
       cmdArray = msg.content.substring(1).split()
       cmd = cmdArray[0]
       args = cmdArray.slice(1)
