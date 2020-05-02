b0f1092fc27fd83d832bc6cf5620efffefc554fd
Furaneau <Furaneau@users.noreply.github.com>
2020-05-02 05:41:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({DisableEveryone: true});
 
 bot.on('ready', async () =>{
   console.log('Le bot est lancé.');
-  bot.user.setActivity('Papy Gelus raconte nous une histoire');
+  bot.user.setActivity('Papy Gelus raconte nous une histoire !!!');
 });
 
 bot.login(process.env.ContinentalCraft);
