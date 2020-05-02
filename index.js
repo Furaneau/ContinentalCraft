0324cb276f98853f49ae4f0974a64f346a252011
Furaneau <Furaneau@users.noreply.github.com>
2020-05-02 05:31:18 +0200

    fixe papy index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client({DisableEveryone: true});
 
 bot.on('ready', async () =>{
-  bot.user.setActivity('Hey hey hey');
+  bot.user.setActivity('Papy Gelus raconte nous une histoire');
 });
 
 bot.login(process.env.ContinentalCraft);
