a4b6f7de97fbbac27c07f902f16980f519869da7
Furaneau <Furaneau@users.noreply.github.com>
2020-05-02 16:29:58 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,19 +1,51 @@
 module.exports = async (client, message, args) => {
-client.on('message', function (message) {
-    if (!message.guild) return
-    let args = message.content.trim().split(/ +/g)
 
-    if (args[0].toLowerCase() === prefix + 'kick') {
-       if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send("Vous navez pas la permission d utiliser cette commande");
-       let member = message.mentions.members.first()
-       if (!member) return message.channel.send("Veuillez mentionner un utilisateur :x:")
-       if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("Vous ne pouvez pas kick cet utilisateur :x:")
-       if (!member.kickable) return message.channel.send("Je ne peux pas exclure cet utilisateur :sunglass:")
-       member.kick()
-       message.channel.send('**' + member.user.username + '** a été exclu :white_check_mark:')
-     }
+client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
+
+  // If the message content starts with "!kick"
+  if (message.content.startsWith('!kick')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member
+          .kick('Optional reason that will display in the audit logs')
+          .then(() => {
+            // We let the message author know we were able to kick the person
+            message.reply(`Successfully kicked ${user.tag}`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to kick the member');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply("That user isn't in this guild!");
+      }
+      // Otherwise, if no user was mentioned
+    } else {
+      message.reply("You didn't mention the user to kick!");
     }
+  }
 });
+}
+
 
 module.exports.help = {
   name: 'kick'
