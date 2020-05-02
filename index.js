d43be037a06bbce0adfd235373815f4c9ff9d6dd
Furaneau <Furaneau@users.noreply.github.com>
2020-05-02 13:58:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,24 +20,24 @@ bot.on('message', async (msg) => {
     }
 });
 
-module.exports.run = async(client, message, args) => {
+bot.on('message', async (msg) => {
 
-  if(!message.guild.member(message.author).hasPermission("KiCK_MEMBERS")) return message.channel.send(`Vous avez pas les perm`);
+  if(!msg.guild.member(msg.author).hasPermission("KiCK_MEMBERS")) return msg.channel.send(`Vous avez pas les perm`);
 
-  if(message.mention.user.size === 0) {
-    return message.channel.send(`Aucune personne`);
+  if(msg.mention.user.size === 0) {
+    return msg.channel.send(`Aucune personne`);
   }
 
-  let kick = message.guild.member(message.mention.user.first())
+  let kick = msg.guild.member(msg.mention.user.first());
 
   if(!kick){
-    return message.channel.send(`Pas de joueur`);
+    return msg.channel.send(`Pas de joueur`);
   }
 
-  if(!message.guild.member(client.user).hasPermission("KiCK_MEMBERS"))return message.channel.send(`Manque de permision`);
+  if(!msg.guild.member(client.user).hasPermission("KiCK_MEMBERS"))return msg.channel.send(`Manque de permision`);
 
   kick.kick().then(member => {
-    message.channel.send(`${member.user.username} est kick par ${message.author.username}`);
+    msg.channel.send(`${msg.user.username} est kick par ${msg.author.username}`);
   });
 };
 
