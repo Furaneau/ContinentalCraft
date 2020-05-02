add49e187d43318ee77cadcc97ddb16b4c296f24
Furaneau <Furaneau@users.noreply.github.com>
2020-05-02 15:59:11 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,36 +1,17 @@
 module.exports.run = async (bot, msg, args) => {
 
-  if (message.content.startsWith('!kick')) {
+  client.on('message', function (message) {
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
 
-    const user = message.mentions.users.first();
-
-    if (user) {
-
-      const member = message.guild.member(user);
-
-      if (member) {
-
-        member
-          .kick('Optional reason that will display in the audit logs')
-          .then(() => {
-
-            message.reply(`Successfully kicked ${user.tag}`);
-          })
-          .catch(err => {
-
-            message.reply('I was unable to kick the member');
-
-            console.error(err);
-          });
-      } else {
-
-        message.reply("That user isn't in this guild!");
-      }
-    } else {
-      message.reply("You didn't mention the user to kick!");
+    if (args[0].toLowerCase() === prefix + 'kick') {
+       if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande ;(")
+       let member = message.mentions.members.first()
+       if (!member) return message.channel.send("Veuillez mentionner un utilisateur :x:")
+       if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("Vous ne pouvez pas kick cet utilisateur :x:")
+       if (!member.kickable) return message.channel.send("Je ne peux pas exclure cet utilisateur :sunglass:")
+       member.kick()
+       message.channel.send('**' + member.user.username + '** a été exclu :white_check_mark:')
     }
-    module.exports.help = {
-      name: 'kick'
-    }
-  }
-});
+})
+}
