4601dfcc060890b38f6cb668bc8f5264d12554ed
Furaneau <Furaneau@users.noreply.github.com>
2020-05-02 23:10:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({DisableEveryone: true});
 const config = require('./config.js');
 const commands = require('./commands.js');
 
-bot.on('guildMemberAdd', membre => {
+bot.on('guildMemberAdd', async (msg) => {
   member.guild.channel.get('705549754167328812').send('Bienvenue');
   console.log('+1')
 });
