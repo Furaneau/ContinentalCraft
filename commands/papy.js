80061bf71099e34a039fcbef52a7de285689fa3a
Furaneau <Furaneau@users.noreply.github.com>
2020-05-03 11:06:53 +0200

    Update papy.js

--- commands/papy.js
+++ commands/papy.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js');
+module.exports.run = async (bot, msg, args) => {
 var bot = new Discord.Client();
 bot.on('message', function (message) {
   if (message.content == '.Papy') {
     message.reply("Papy gelus");
   }
 });
-
+}
 module.exports.help = {
   name: 'Papy'
 }
