2740aba80a9a29a17ffe716a3fb300345148e7cd
Furaneau <Furaneau@users.noreply.github.com>
2020-05-03 03:50:54 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,16 +1,27 @@
-module.exports.run = async (bot, msg, args) => {
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
 
-  let member = message.mentions.members.first();
- member.kick().then((member) => {
-     message.channel.send(`:wave: ${member.displayName} has been kicked`);
- }).catch(() => {
-     if (!message.member.hasPermission(['KICK_MEMBERS', 'Admin'])) {
-         message.reply("You cannot kick members");
-     } else if (member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS', 'Admin'])) {
-         message.reply("You cannont kick this member");
-     }
- })
-}
+  if(message.author.bot) return;
+  message.delete()
+  if(!message.guild.member(message.author).hastPermission("KICK_MEMBERS")) return;
+  const user = message.mentions.users.first();
+  if(user.id === message.author.id) return message.channel.send("Vous pouvez pas vous kick")
+  if(user) {
+    const member = message.guild.member(user)
+    if(member) {
+      member.kick(' ').then(() => {
+        message.rely(`${user.tag} a été kick`)
+      }).catch(err => {
+        message.reply("Vous n'avez pas les perm")
+      })
+
+    }else {
+      message.reply("Cet personne est pas sur le serveur")
+    }
+    }else {
+    message.reply("Vous avez oublier de mentioner une personne")
+  }
+  }
 
   module.exports.help = {
     name: 'kick'
